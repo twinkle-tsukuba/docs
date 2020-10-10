24f05009ac5406544c7dfb939f4a388202035d0a
Kazumi Inada <hello@nandenjin.com>
2020-10-10 12:23:02 +0900

    chore: 不要なimportの削除

--- src/.vuepress/theme/layouts/IndexNews.vue
+++ src/.vuepress/theme/layouts/IndexNews.vue
@@ -3,15 +3,3 @@
     <div>IndexNews</div>
   </div>
 </template>
-
-<script>
-import NavBar from '@parent-theme/components/NavBar.vue'
-import SideBar from '@parent-theme/components/SideBar.vue'
-
-export default {
-  components: {
-    NavBar,
-    SideBar,
-  },
-}
-</script>
