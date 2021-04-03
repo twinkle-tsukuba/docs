af6a28200c39f45043fd4b0c7cb9487d213d6330
Kazumi Inada <hello@nandenjin.com>
2021-04-03 22:09:58 +0900

    chore: Newsページのデザイン修正

--- src/.vuepress/theme/layouts/News.vue
+++ src/.vuepress/theme/layouts/News.vue
@@ -4,12 +4,14 @@
 
     <main class="theme-default-content">
       <h1>{{ $page.title }}</h1>
-      {{ $frontmatter.date }}
-      <ul>
-        <li v-for="tag of $frontmatter.tags" :key="tag">
-          <router-link :to="`/tags/${tag}`">{{ tag }}</router-link>
-        </li>
-      </ul>
+      <div class="article-info">
+        <span class="publish-date">{{ publishDateString }}</span>
+        <ul class="tag-list">
+          <li class="tag-list__item" v-for="tag of $frontmatter.tags" :key="tag">
+            <router-link :to="`/tags/${tag}`">{{ tag }}</router-link>
+          </li>
+        </ul>
+      </div>
       <Content />
     </main>
   </div>
@@ -22,5 +24,47 @@ export default {
   components: {
     Navbar,
   },
+  computed: {
+    publishDateString() {
+      const dateStr = this.$frontmatter.date
+      if (dateStr) {
+        const d = new Date(dateStr)
+        return `${d.getFullYear()}.${(d.getMonth() + 1)}.${d.getDate()}`
+      } else {
+        return null
+      }
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.article-info {
+  margin: 15px 0 25px;
+}
+
+.publish-date {
+  font-size: 15px;
+  margin-right: 10px;
+}
+
+.tag-list {
+  display: inline-block;
+  list-style: none;
+  padding: 0;
+
+  &__item {
+    display: inline-block;
+
+    a {
+      padding: 3px 5px;
+      margin-right: 5px;
+      background-color: #eee;
+      color: #3eaf7c;
+      font: normal 13px monospace;
+      line-height: 1em;
+      display: inline-block;
+    }
+  }
+}
+</style>
