ec34ea9d47834a00526c73123af5ba0756f902c2
Kazumi Inada <hello@nandenjin.com>
2021-04-04 01:16:01 +0900

    feat: newsのpublishdateを表示する

--- src/.vuepress/theme/layouts/IndexNews.vue
+++ src/.vuepress/theme/layouts/IndexNews.vue
@@ -5,7 +5,7 @@
       <h1>お知らせ</h1>
       <ul>
         <li v-for="page of $pagination.pages" :key="page.key">
-          <a :href="page.path">{{ page.title }}</a>
+          <a :href="page.path"><span class="publish-date">{{ formatDate(page.frontmatter.date) }}</span><span>{{ page.title }}</span></a>
         </li>
       </ul>
     </main>
@@ -19,5 +19,20 @@ export default {
   components: {
     Navbar,
   },
+  methods: {
+    formatDate(dateStr) {
+      const d = new Date(dateStr)
+      return `${d.getFullYear()}.${(d.getMonth() + 1)}.${d.getDate()}`
+    }
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+.publish-date {
+  display: inline-block;
+  color: #444;
+  margin-right: 0.5em;
+  font-size: 0.9em;
+}
+</style>
